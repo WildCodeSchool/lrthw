816a7595c568e4a6aeb8609cb5fab4f89099a7c9
jgrom <jgromdev@gmail.com>
2016-07-23 20:27:59 +0200

    Terminé exercice 03b - study drills

---
+++ ex03b.rb
@@ -0,0 +1,10 @@
+puts "Eight hundred hens lay eggs on average eight hundred in eight days."
+puts "How many eggs lay four hundred hens in four days ?"
+
+puts "..."  
+
+puts "If 800 hens lay eggs on average 800 in 8 days,"
+puts "#{800/2} hens lay eggs on average #{800/2} in 8 days,"
+puts "so, #{800/2} hens lay eggs on average #{(800/2)/2} in #{8/2} days."
+
+puts "Solution: #{(800/2)/2} eggs !"
