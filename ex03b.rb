48bd0a49f8eb24568be461244e502a7c136e80a7
jgrom <jgromdev@gmail.com>
2016-07-23 20:32:15 +0200

    Terminé exercice 3b - study drills

--- ex03b.rb
+++ ex03b.rb
@@ -1,5 +1,5 @@
 puts "Eight hundred hens lay eggs on average eight hundred in eight days."
-puts "How many eggs lay four hundred hens in four days ?"
+puts "How many eggs lay four hundred hens in four days?"
 
 puts "..."  
 
@@ -7,4 +7,4 @@ puts "If 800 hens lay eggs on average 800 in 8 days,"
 puts "#{800/2} hens lay eggs on average #{800/2} in 8 days,"
 puts "so, #{800/2} hens lay eggs on average #{(800/2)/2} in #{8/2} days."
 
-puts "Solution: #{(800/2)/2} eggs !"
+puts "Solution: #{(800/2)/2} eggs!"
